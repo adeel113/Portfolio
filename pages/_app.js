d085a3e47f6dfe5f4c1a089dd64989a39187064b
Adeel <mohammad.adeel@graffitecs.com>
2023-09-01 22:25:24 +0500

    testing

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
 
+
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
